1d0ab86718872e478ba974e1cf1eaff47fe963d8
kathrin <kathrin.koenig@htl-salzburg.ac.at>
2020-01-13 10:36:22 +0100

    Added Sprite for Ball, added MovingBall Script, Activated by Click on Button

---
+++ Assets/MyGame/Scripts/MoveBall.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    public Transform[] positions;
+    public Transform ball;
+    public void MovingBall()
+    {
+        ball.position = positions[Random.Range(0, positions.Length)].position;
+    }
+}
+
